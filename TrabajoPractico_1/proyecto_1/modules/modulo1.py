be3cbbfd5c46819d18794f9ca9c4f7e89f3705f3
AndreaLaval <lavalandrea7@gmail.com>
2025-03-12 12:10:29 -0300

    .

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,7 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+def nombre():
+    return "nombre"
+
+def numero():
+    return 1403
